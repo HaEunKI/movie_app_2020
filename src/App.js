c192356711fca906d7d8a155518453976ca7d9e7
HaEunKI <gkdms3082@naver.com>
2022-04-07 18:52:03 +0900

    4장 소스코드 완성일까나

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import propTypes from 'prop-types';
 
-function App() {
+function Food({ name, picture,rating }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>i like {name}</h2>
+      <h4>{rating}/5.9</h4>
+      <img src={picture} alt={name}/>
     </div>
   );
 }
 
+const foodILike = [
+  {
+    id: 1,
+    name:'Kimchi',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.4aBRBEMc36CPZmEg84X40wEsDI%26pid%3DApi&f=1',
+    rating: 1,
+  },
+  {
+    id: 2,
+    name:'Samgyeopsal',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.2QL4k0caBUvTsiuEGRfqzwHaE8%26pid%3DApi&f=1',
+    rating: 2,
+  },
+  {
+    id: 3,
+    name:'Bibimbap',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.XQIww0eJrl8VSqLyJUCWeAHaE8%26pid%3DApi&f=1',
+    rating: 3,
+  },
+  {
+    id: 4,
+    name:'Doncasu',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DxkwyyLp8y5OyBlvX2xbXwHaEO%26pid%3DApi&f=1',
+    rating: 4,
+  },
+  {
+    id: 5,
+    name:'Kimbap',
+    imgae:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.M5YxC0U5LJPxJw__yi9oJgHaGL%26pid%3DApi&f=1',
+    rating: 5,
+  },
+];
+
+function App() {
+  return(
+    <div>
+      { foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.name} rating={dish.rating}/>
+      ))}
+    </div>
+  )
+}
+
+Food.propTypes = {
+  name:propTypes.string.isRequired,
+  picture:propTypes.string.isRequired,
+  rating:propTypes.number.isRequired,
+};
 export default App;
